c037e6e5cd87be36642f3c0201c5e49c99c3307d
lianxiang <1161851688@qq.com>
2019-12-05 17:20:20 +0800

    线程同步

---
+++ WinAPI/DemoMFC09/DemoMFC09.h
@@ -0,0 +1,32 @@
+﻿
+// DemoMFC09.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CDemoMFC09App:
+// 有关此类的实现，请参阅 DemoMFC09.cpp
+//
+
+class CDemoMFC09App : public CWinApp
+{
+public:
+	CDemoMFC09App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDemoMFC09App theApp;
---
+++ WinAPI/DemoMFC09/DemoMFC09Dlg.cpp
@@ -0,0 +1,203 @@
+﻿
+// DemoMFC09Dlg.cpp: 实现文件
+//
+
+#include "stdafx.h"
+#include "DemoMFC09.h"
+#include "DemoMFC09Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CDemoMFC09Dlg 对话框
+
+
+
+CDemoMFC09Dlg::CDemoMFC09Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DEMOMFC09_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CDemoMFC09Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CDemoMFC09Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CDemoMFC09Dlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_BUTTON2, &CDemoMFC09Dlg::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CDemoMFC09Dlg 消息处理程序
+
+BOOL CDemoMFC09Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CDemoMFC09Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CDemoMFC09Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CDemoMFC09Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+#define MAX_NUM 100000000
+int k = 1;
+int total = 0;
+
+UINT ThreadProcA(LPVOID pParam) {
+	::SetDlgItemText(AfxGetApp()->m_pMainWnd->m_hWnd,
+		IDC_OUTPUTA, TEXT("线程A启动"));
+	for (int i = 1; i <= MAX_NUM; ++i) {
+		//加锁
+		k = k * 2;
+		k = k / 2;
+		total += k;
+		//解锁
+	}
+
+	::SetDlgItemInt(AfxGetApp()->m_pMainWnd->m_hWnd,
+		IDC_OUTPUT, total, false);
+	return 0;
+}
+UINT ThreadProcB(LPVOID pParam) {
+	::SetDlgItemText(AfxGetApp()->m_pMainWnd->m_hWnd,
+		IDC_OUTPUTB, TEXT("线程B启动"));
+	for (int j = 1; j <= MAX_NUM; ++j) {
+		//加锁
+		k = k * 2;
+		k = k / 2;
+		total += k;
+		//解锁
+	}
+
+	::SetDlgItemInt(AfxGetApp()->m_pMainWnd->m_hWnd,
+		IDC_OUTPUT, total, false);
+	return 0;
+}
+
+void CDemoMFC09Dlg::OnBnClickedButton1()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	AfxBeginThread(ThreadProcA, NULL);
+	AfxBeginThread(ThreadProcB, NULL);
+}
+
+void CDemoMFC09Dlg::OnBnClickedButton2()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	AfxBeginThread(ThreadProcB, NULL);
+}
