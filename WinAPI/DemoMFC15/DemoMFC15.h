80abb3454d3ba4adce27d9ac9b8ad794723552d0
lianxiang <1161851688@qq.com>
2019-12-06 17:57:44 +0800

    进程通信，通过剪切板通信

---
+++ WinAPI/DemoMFC15/DemoMFC15.h
@@ -0,0 +1,32 @@
+﻿
+// DemoMFC15.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CDemoMFC15App:
+// 有关此类的实现，请参阅 DemoMFC15.cpp
+//
+
+class CDemoMFC15App : public CWinApp
+{
+public:
+	CDemoMFC15App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDemoMFC15App theApp;
---
+++ WinAPI/DemoMFC16/DemoMFC16.h
@@ -0,0 +1,32 @@
+﻿
+// DemoMFC16.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CDemoMFC16App:
+// 有关此类的实现，请参阅 DemoMFC16.cpp
+//
+
+class CDemoMFC16App : public CWinApp
+{
+public:
+	CDemoMFC16App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDemoMFC16App theApp;
